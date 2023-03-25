22a2d76f20dc9205ec2e9b752a5736dc1e949447
Mitch Henline <rmhenline@gmail.com>
2023-03-25 17:08:22 -0600

    building login

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 """Kredible server."""
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from jinja2 import StrictUndefined
 from model import connect_to_db, db
 from forms import LoginForm
@@ -13,17 +13,22 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def homepage():
     """View homepage."""
-
+    if 'user_id' not in session:
+        return redirect('/login')
 
 
     return render_template("home.html")
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """Login page."""
     form = LoginForm(request.form)
 
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
 
+        user = #create crud.py next
     return render_template("login.html", form = form)
 
 if __name__ == "__main__":
