1d3681f23b4316dc7f9a2c12bb8ceeb1d08560dd
Mitch Henline <rmhenline@gmail.com>
2023-03-31 11:22:54 -0600

    useradv model added

--- model.py
+++ model.py
@@ -22,7 +22,7 @@ class User(db.Model):
         return f'User: id={self.id} Name={self.last_name, self.first_name} Company={self.company}'
     
 class Advocate(db.Model):
-    """An User's Advocate."""
+    """An user's advocate."""
 
     __tablename__ ="advocates"
 
@@ -40,6 +40,17 @@ class Advocate(db.Model):
         return f'Advocate: Advocate_ID={self.id} Name={self.last_name, self.first_name} Company={self.company}'
 
 
+class UserAdv(db.Model):
+    """User-advocate relationship table"""
+
+    __tablename__ = "user_adv_relationships"
+
+    user_adv = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    adv_id = db.Column(db.Integer, db.ForeignKey("advocates.adv_id"))
+
+    user = db.relationship("User", backref="user_adv_relationships")
+    advocate = db.relationship("Advocate", backref="user_adv_relationships")
 
 def connect_to_db(flask_app, db_uri=os.environ["POSTGRES_URI"], echo=False):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
