3ff54a64c6250776c3098026296ca3f9079bd0cf
Mitch Henline <rmhenline@gmail.com>
2023-04-25 20:08:34 -0600

    good js progress with displaying advs

--- static/js/advocates.js
+++ static/js/advocates.js
@@ -1,4 +1,17 @@
-function showDiv() {
+function showAdvInfo(advocate) {
     let div = document.getElementById("myDiv");
+    div.innerHTML = `
+      <button id="closeButton" style="float: right;">X</button>
+      <h1>${advocate.first_name} ${advocate.last_name}</h1>
+      <p>Company: ${advocate.company}</p>
+      <p>Email: ${advocate.email}</p>
+      <p>Industry: ${advocate.industry}</p>
+      <p>Company Size: ${advocate.company_size}</p>
+    `;
     div.style.display = "block";
+  
+    document.getElementById("closeButton").addEventListener("click", function() {
+      div.style.display = "none";
+    });
   }
+  
